32e05922a69c526c3b76392868f8eb40b280771a
Jan Ťulák <github@tulak.me>
2013-10-22 22:52:44 +0200

    Added comments to the thread test and set better default values.

--- tests/test-threads.sh
+++ tests/test-threads.sh
@@ -2,19 +2,30 @@
 # Will run BIN rdrand test with different number of threads.
 # If second argument THROUGHPUT_FILE is given,
 # then output of the test will be written also to the file 
-# as JSON.
+# as JSON. Will overwrite file if exists.
 
 BIN="./RdRand"
 
-MIN=1
-MAX=3
+# Minimum and maximum count of threads tested
+MIN=1 
+MAX=16
 
-DURATION=1
+# Duration of each single tested method
+DURATION=3
+
+# size of array of 64bit values to be generated at once
 CHUNK=2048
-REPETITION=1
+
+# How many times should be the test run for all methods 
+# before computing an average throughput
+REPETITION=3
+
+
+##################################################################
+# START
 
 if [ $# -lt 1 ] || [ $# -gt 2 ]; then 
-  echo "Usage: $0 RNG_OUTPUT_FILE [THROUGHPUT_FILE]"
+  echo "Usage: $0 RNG_OUTPUT_FILE [THROUGHPUT_FILE] (files will be overwritten)"
   exit 1
 fi
 
@@ -31,7 +42,7 @@ echo "Will test from $MIN to $MAX threads."
 for ((THREADS=$MIN ; THREADS<=$MAX ; THREADS++ )); do
   echo "Currently testing $THREADS threads."
   
-  CMD="'$BIN' '$FILE_STDOUT' -d $DURATION -c $CHUNK -r $REPETITION -t $THREADS -m rdrand16_step "
+  CMD="'$BIN' '$FILE_STDOUT' -d $DURATION -c $CHUNK -r $REPETITION -t $THREADS"
   if [ -z "$THROUGHPUT_FILE"  ]; then
     eval $CMD
   else
